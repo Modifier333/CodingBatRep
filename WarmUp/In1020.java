ed5e145112fbfa80bf129a74090c3973f4a12ac0
root <sony.123@gmail.com>
2023-04-06 22:31:14 +0530

    first

---
+++ WarmUp/In1020.java
@@ -0,0 +1,16 @@
+package Warmup1;
+
+public class In1020 {
+    public static void main(String[] args) {
+
+        int a=9;
+        int b=19;
+        System.out.println(m(a,b));
+    }
+    public static boolean m(int a, int b){
+if((a>=10&&a<=20)||(b>=10&&b<=20)){
+    return true;
+}
+        return false;
+    }
+}
