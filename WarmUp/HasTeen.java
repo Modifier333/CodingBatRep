f114b35ad0debbf97facb3d0eb2e9cb3ad71bb98
root <sony.123@gmail.com>
2023-04-08 23:15:34 +0530

    first commit

---
+++ WarmUp/HasTeen.java
@@ -0,0 +1,17 @@
+package Warmup1;
+
+public class HasTeen {
+    public static void main(String[] args) {
+        int a=13;
+        int b=10;
+        int c=20;
+        System.out.println(m(a,b,c));
+    }
+    public static boolean m(int a,int b,int c){
+        if((a>=13&&a<=19)||(a>=13&&a<=19)||(a>=13&&a<=19)){
+return true;
+        }else{
+            return false;
+        }
+    }
+}
