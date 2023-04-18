e522cde180c72c726a7bb900347c5f89035e1b30
root <sony.123@gmail.com>
2023-04-18 23:11:46 +0530

    second

--- WarmUp2/FrontTimes.java
+++ WarmUp2/FrontTimes.java
@@ -1,61 +1,30 @@
-<<<<<<< HEAD
-package WarmUp2;
+package Warmup2;
 
 public class FrontTimes {
-
-
     public static void main(String[] args) {
-        int n=4;
-        String str="ab";
+        String str="he";
+        int n=5;
+
         System.out.println(m(str,n));
     }
-    public static String m(String str,int n){
-        String s="";
-        if(str.length()>=3){
-            for (int i=0;i<n;i++){
-                s+=str.substring(0,3);
+    static String m(String str,int n){
+String s="";
 
+        if(str.length()>=3){
+           String s1=str.substring(0,3);
+            for(int i=0;i<n;i++){
+                s=s+s1;
             }
             return s;
-        }else{
-            for(int i=0;i<n;i++){
-                s+=str;
+        }
+        else{
+            while(n>0){
+                s=s+str;
+                n--;
             }
             return s;
         }
 
 
     }
-
 }
-=======
-package WarmUp2;
-
-public class FrontTimes {
-
-
-    public static void main(String[] args) {
-        int n=4;
-        String str="ab";
-        System.out.println(m(str,n));
-    }
-    public static String m(String str,int n){
-        String s="";
-        if(str.length()>=3){
-            for (int i=0;i<n;i++){
-                s+=str.substring(0,3);
-
-            }
-            return s;
-        }else{
-            for(int i=0;i<n;i++){
-                s+=str;
-            }
-            return s;
-        }
-
-
-    }
-
-}
->>>>>>> aa0c4a1f85b93f449d024968e073390b7f94bed6
